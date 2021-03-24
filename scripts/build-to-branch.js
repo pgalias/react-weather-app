37ea06c36803c19afc57e3c1b00075c4e857453d
pgalias <pawel@galias.me>
2021-03-25 00:54:15 +0100

    chore: change main to master in build-to-branch script

--- scripts/build-to-branch.js
+++ scripts/build-to-branch.js
@@ -12,7 +12,7 @@ const execa = require('execa');
     console.log('Pushing to gh-pages...');
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force', '--no-verify']);
     await execa('rm', ['-r', folderName]);
-    await execa('git', ['checkout', '-f', 'main']);
+    await execa('git', ['checkout', '-f', 'master']);
     await execa('git', ['branch', '-D', 'gh-pages']);
     console.log('Successfully deployed');
   } catch (e) {
